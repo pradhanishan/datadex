4ea85e59a76cc409d5df1bd724d9a34a32531882
Ishan Pradhan <ishanpradhan.ip@gmail.com>
2024-04-23 22:26:52 +0545

    reset header

--- app/(protected)/dashboard/layout.tsx
+++ app/(protected)/dashboard/layout.tsx
@@ -1,12 +1,3 @@
-import { Header } from '@/components/header/header';
-
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="min-h-screen">
-      <Header />
-      {/* To keep a gap between fixed header and dashboard content */}
-      <div className="h-[80px]"></div>
-      {children}
-    </div>
-  );
+  return <div className="min-h-screen">{children}</div>;
 }
