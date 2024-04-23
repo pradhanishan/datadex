1aec20e443ce4981249af371fb71f6c674349049
Ishan Pradhan <ishanpradhan.ip@gmail.com>
2024-04-23 14:50:43 +0545

    add hook jsdoc

--- hooks/use-current-user.ts
+++ hooks/use-current-user.ts
@@ -1,5 +1,9 @@
 import { useSession } from 'next-auth/react';
 
+/**
+ *
+ * A custom hook to get current user from the active session
+ */
 export default function useCurrentUser() {
   const session = useSession();
   return session.data?.user;
