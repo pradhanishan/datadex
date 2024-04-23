9a90f7a64315714a9f8b09c7e03aa1247d695b6d
Ishan Pradhan <ishanpradhan.ip@gmail.com>
2024-04-23 09:53:19 +0545

    create register server action

--- actions/auth-actions/register-action.ts
+++ actions/auth-actions/register-action.ts
@@ -1,8 +1,33 @@
 'use server';
 
+import prisma from '@/lib/db';
 import { RegisterSchema } from '@/schemas';
+import bcrypt from 'bcryptjs';
 import { z } from 'zod';
 
 export async function register(formData: z.infer<typeof RegisterSchema>) {
-  console.log(formData);
+  try {
+    const validatedFields = RegisterSchema.safeParse(formData);
+    if (!validatedFields.success) {
+      return { error: 'invalid credentials' };
+    }
+
+    const { username, email, password } = formData;
+
+    const existingUser = await prisma.user.findFirst({ where: { OR: [{ email }, { name: username }] } });
+    if (existingUser) {
+      if (existingUser.email === email) {
+        return { error: 'You have already registered with this email' };
+      }
+      return { error: `username ${username}} is taken` };
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const createdUser = await prisma.user.create({ data: { name: username, email, hashedPassword } });
+    return { success: 'verification email sent' };
+  } catch (error) {
+    console.log(error);
+    return { error: 'An internal server error occurred' };
+  }
 }
